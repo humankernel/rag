2c71887654a8045bcd0958f8bbaed0640d266b91
strange <rivasjoaquin02@gmail.com>
2025-03-17 04:08:01 -0400

    lets see

--- src/rag/settings.py
+++ src/rag/settings.py
@@ -1,5 +1,4 @@
 import os
-
 import torch
 from dotenv import load_dotenv
 from pydantic import BaseModel, computed_field
@@ -16,21 +15,20 @@ class Model(TypedDict):
 class Settings(BaseModel):
     """Configuration for the app"""
 
-    # Paths
     EMBEDDING_MODEL: str = os.getenv("EMBEDDING_MODEL", "BAAI/bge-m3")
     EMBEDDING_TOKEN_LIMIT: int = 8190
     RERANKER_MODEL: str = os.getenv("RERANKER_MODEL", "BAAI/bge-reranker-v2-m3")
-    LLM_MODEL: str = os.getenv("LLM_MODEL", "meta-llama/Llama-3.2-3B-Instruct")
+    LLM_MODEL: str = os.getenv("LLM_MODEL", "DeepSeek-R1-Distill-Qwen-1.5B-Q8_0.gguf")
     CTX_WINDOW: int = 8192
-
-    # General models
-    TORCH_DEVICE: Optional[Literal["cuda", "cpu"]] = None
 
     # List of additional models with names and optional paths
     MODELS: list[Model] = [
-        {"name": "DeepSeek R1 Destill", "path": os.getenv("DEEPSEEK_PATH")},
-        {"name": "Qwen2.5 Instruct", "path": os.getenv("QWEN_PATH")},
+        {"name": "DeepSeek-R1-Distill-Qwen-1.5B-GGUF", "path": os.getenv("DEEPSEEK_PATH")},
+        {"name": "Qwen2.5-Instruct", "path": os.getenv("QWEN_PATH")},
     ]
+
+    # General models
+    TORCH_DEVICE: Optional[Literal["cuda", "cpu"]] = None
 
     @computed_field
     @property
